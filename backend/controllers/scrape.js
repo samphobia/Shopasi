443cfbe1b8b7ed8e30dd9998809cdb0e6038b0bd
Samuel Osinubi <samobafemisayo@gmail.com>
2023-01-26 17:34:16 +0100

    integrated controller

--- backend/controllers/scrape.js
+++ backend/controllers/scrape.js
@@ -1,3 +1,24 @@
 const express = require('express')
 const request = require('request')
 const cheerio = require('cheerio')
+const Data = require('../models/data')
+
+exports.scrape = (req, res, next) => {
+  const siteUrl = req.body.siteUrl
+  request(siteUrl, (error, response, html) => {
+    if (!error && response.statusCode == 200) {
+      const $ = cheerio.load(html)
+      $('.product-name').each((i, element) => {
+        const name = $(element).text()
+        const price = $(element).parent().find('.product-price').text()
+
+        const data = new Data({
+          name: name,
+          price: price
+        })
+
+        data.save()
+      })
+    }
+  })
+}
